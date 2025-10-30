64956eee6026bd6d078a81afab660a7c943a6b5f
nilswag <nilswagemaker05@icloud.com>
2025-10-30 12:34:47 +0100

    Changed CMakeLists and gitignore to work better with clangd lsp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+#include <iostream> 
 
 int main(void)
 {
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << BASE_PATH << std::endl;
+    return 0;
 }
